25030e5238b713af686be286bb2518e182807f04
jorn <chenbingweb@sina.com>
2018-10-17 23:14:44 +0800

    1

---
+++ web-mobile/src/settings.b01c4.js
@@ -0,0 +1,127 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default",
+        "edge",
+        "pocket",
+        "cue",
+        "ball",
+        "white_ball"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ],
+        [
+            false,
+            false,
+            null,
+            null,
+            true,
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            null,
+            true,
+            true
+        ],
+        [
+            false,
+            false,
+            false,
+            false,
+            null,
+            true
+        ],
+        [
+            false,
+            true,
+            true,
+            false,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            true,
+            true,
+            true,
+            false
+        ]
+    ],
+    rawAssets: {
+        assets: {
+            "f7KSwBkPVKVb6fQfMWTrTQ": [
+                "res/bbbj.png",
+                0
+            ],
+            "6c8WCPMcVAGqYhHMkD7v3P": [
+                "res/qiuzhuo.png",
+                0
+            ],
+            "ebCJwgXKVIiaHyrby6lRUB": [
+                "res/sprite_sheet.png",
+                0
+            ]
+        },
+        internal: {
+            "71VhFCTINJM6/Ky3oX9nBT": [
+                "image/default_btn_disabled.png",
+                0
+            ],
+            "e8Ueib+qJEhL6mXAHdnwbi": [
+                "image/default_btn_normal.png",
+                0
+            ],
+            "b4P/PCArtIdIH38t6mlw8Y": [
+                "image/default_btn_pressed.png",
+                0
+            ]
+        }
+    },
+    assetTypes: [
+        "cc.Texture2D"
+    ],
+    launchScene: "db://assets/scences/game_scence.fire",
+    scenes: [
+        {
+            url: "db://assets/scences/game_scence.fire",
+            uuid: 0
+        }
+    ],
+    packedAssets: {
+        "06efbe01f": [
+            "13C45e8V1J16ovyCTUEA/f",
+            "29FYIk+N1GYaeWH/q1NxQO",
+            "31+hYtXApJMoNod4q4Dxd1",
+            "35hlxkOvJGzrO1I+aw7Mi+",
+            "597Zh4Wp5AQ5aWUDBN+kDk",
+            "7awI3EQBdJwJi7Qg1HBd/e",
+            "b3FDTJnAJIer2eXl/s6wJL",
+            "d7rebA04hGGInEh8n6xrDv",
+            "e97GVMl6JHh5Ml5qEDdSGa",
+            "ecIvlGIy1NFLeU8f7Rdw6C",
+            0,
+            "edLdPm7w5Lu5my4gQQ8ygJ",
+            "f0BIwQ8D5Ml7nTNQbh1YlS",
+            "f7MOWcT3ZEFbKmQg/hPDVQ"
+        ]
+    },
+    orientation: "portrait",
+    uuids: [
+        "edJ2G/m4RK4ra4GmgGSkh8"
+    ],
+    "md5AssetsMap": {
+        "06/06efbe01f.json": "4933e",
+        "assets/res/bbbj.png": "b6e58",
+        "assets/res/qiuzhuo.png": "e3d04",
+        "assets/res/sprite_sheet.png": "d8bee",
+        "internal/image/default_btn_disabled.png": "286c6",
+        "internal/image/default_btn_normal.png": "1ecb7",
+        "internal/image/default_btn_pressed.png": "bedf4"
+    }
+};
